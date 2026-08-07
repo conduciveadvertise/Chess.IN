a7bcd99af6be4eabb7528cbea78aaf710e0cdde8
Mohammad faisal <faisalmd44@gmail.com>
2026-08-07 15:26:43 +0530

    Fix: Switched jsEngine to Hermes and updated babel config

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      require.resolve('expo-router/babel'),
-      'react-native-reanimated/plugin',
+      'react-native-reanimated/plugin'
     ],
   };
 };
+
